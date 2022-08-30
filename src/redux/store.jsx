d7fbeb5222cf6daf6272cf506fc3efadec163642
KimsanboyevShoxidbek2000 <76057864+KimsanboyevShoxidbek2000@users.noreply.github.com>
2022-08-30 13:14:38 +0500

    hello

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -5,12 +5,15 @@ import deleteModalSlice from './deleteModalSlice/deleteModalSlice';
 // team
 import teamEditModalSlice from './teamEditModalSlice/teamEditModalSlice';
 import teamDeleteModalSlice from './teamDeleteModalSlice/teamDeleteModalSlice';
+// login
+import loginSlice from './loginSlice/loginSlice';
 const store = configureStore({
     reducer: {
         editActions: editModalSlice,
         deleteActions: deleteModalSlice,
         teamEditeActions:teamEditModalSlice,
-        teamDeleteActions:teamDeleteModalSlice
+        teamDeleteActions:teamDeleteModalSlice,
+        loginActions: loginSlice
     }
 })
 
